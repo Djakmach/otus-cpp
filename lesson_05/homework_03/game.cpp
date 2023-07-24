be36e2031171ce7b39b52c6d23e891e0a5153224
Mikhail <mihail_ivanov007@mail.ru>
2023-07-24 23:08:49 +0300

    homework 3

--- lesson_05/homework_03/game.cpp
+++ lesson_05/homework_03/game.cpp
@@ -10,13 +10,15 @@
 #include "game.h"
 
 
-bool is_string_has_spaces(const std::string &str) {   
+std::string name_file_scores = "scores.txt";
+
+bool is_number(const std::string &str) {   
     for (char ch : str) {
-        if (ch == ' ') {
-            return true;
+        if (!std::isdigit(ch)) {
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 bool is_valid_int(const std::string &str) {   
@@ -38,7 +40,7 @@ bool is_number_in_range(const int MIN_NUMBER, const int user_number, const int M
     
 }
 
-bool write_to_score_board(const std::string &name_file_scores, const std::string &user_name, const int attempts) {
+bool write_to_score_board(const std::string &user_name, const int attempts) {
 	std::fstream file{name_file_scores, std::fstream::app};
     if (!file.is_open()) {
         std::cout << "Error! File was not opened!" << std::endl;
@@ -48,7 +50,8 @@ bool write_to_score_board(const std::string &name_file_scores, const std::string
     file.close();
 }
 
-void show_score_board(const std::string &name_file_scores) {
+void show_score_board() {
+    std::cout <<  "High scores table:" << std::endl;
     std::fstream file{name_file_scores, std::fstream::in};
     if (!file.is_open()) {
         std::cout << "Error! File was not opened!" << std::endl;
@@ -61,41 +64,56 @@ void show_score_board(const std::string &name_file_scores) {
     file.close();
 }
 
-int get_arg(int argc, char *argv[]) {
+int get_arg(int argc, char* argv[]) {
     // std::cout << "argc " << argc << '\n';
     // std::cout << "argv_0 " << argv[0] << '\n';
     // std::cout << "argv_1 " << argv[1] << '\n';
     // std::cout << "argv_2 " << argv[2] << '\n';
 
+
     if (argc == 1) {
+
         int default_max_number = 100;
         return default_max_number;
     std::cout << "true " << '\n';
     }
 
-    if (argc == 3) {
-        std::cout << "true " << '\n';
-        std::string key{argv[1]};
-        if (key == "--max") {
+    if (argc > 1) {
+        const char* max = "-max";
+        std::string key = argv[1];
+
+        if (std::strcmp(argv[1], "-max") == 0) {   
             int max_number = std::stoi(argv[2]);
             std::cout << max_number << '\n';
             return max_number;
+        } else if (key == "-table") {
+            show_score_board();
+            return -1;
+        } else {
+            std::cout << "Error! Invakid command.\n";
+            return -1;
         }
     // TODO не обработанная ветка
+    } else {
+        std::cout << "5 " << '\n';
+
+        std::cout << "Error! Invakid command.\n";
+        return -1;
     }
 }
 
 int main(int argc, char *argv[]) {
     std::srand(time(NULL));
-    // const int MAX_NUMBER = get_arg(argc, argv);
-    std::cout << "MAX_NUMBER = " << get_arg(argc, argv) << '\n';
+    const int MAX_NUMBER = get_arg(argc, argv);
+    if (MAX_NUMBER == -1) {
+        return -1;
+    }
+        std::cout << "MAX_NUMBER = " << MAX_NUMBER << '\n';
     const int MIN_NUMBER = 0;
-    const int MAX_NUMBER = 100;
     int hidden_number = std::rand() % MAX_NUMBER;
     std::string user_name;
     std::string user_input;
     int attempts = 1;
-    std::string file_scores = "scores.txt";
     std::string invaition_message = "Enter your guess in range [" + std::to_string(MIN_NUMBER) + ".." +  std::to_string(MAX_NUMBER) + "]";
 
     // std::cout << "Hidden_number " << hidden_number << std::endl;
@@ -107,16 +125,18 @@ int main(int argc, char *argv[]) {
     while (true) {
         std::getline(std::cin, user_input);
         
-        std::cerr << "is_string_has_spaces " << is_string_has_spaces(user_input) << '\n';
+        std::cerr << "is_string_number " << is_number(user_input) << '\n';
         std::cerr << "is_valid_int " << is_valid_int(user_input) << '\n';
-
-        if (!is_valid_int(user_input)) {
-            std::cout << "Error! Invalid input" << "\n";
+        
+        if (!is_number(user_input)) {
+            std::cin.clear();
+            std::cin.ignore(10000,'\n');
+            std::cout << "Error! String is not numeric" << "\n";
             std::cout << invaition_message << std::endl;
             continue;
         }
-        if (is_string_has_spaces(user_input)) {
-            std::cout << "Error! String contain spaces" << "\n";
+        if (!is_valid_int(user_input)) {
+            std::cout << "Error! Number is too long" << "\n";
             std::cout << invaition_message << std::endl;
             continue;
         }
@@ -130,15 +150,14 @@ int main(int argc, char *argv[]) {
         
         if (user_number == hidden_number) {
             std::cout << "you win! attempts = " << attempts << std::endl;
-            write_to_score_board(file_scores, user_name, attempts);
+            write_to_score_board(user_name, attempts);
 
-            std::cout <<  "High scores table:" << std::endl;
-            show_score_board(file_scores);
+            show_score_board();
             break;
         } else if(user_number < hidden_number) {
-            std::cout << "greater than " << user_number << std::endl;
+            std::cout << "greater then " << user_number << std::endl;
         } else {
-            std::cout << "less than " << user_number << std::endl;
+            std::cout << "less then " << user_number << std::endl;
         }
         ++attempts;
     }
