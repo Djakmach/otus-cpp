fb50acd16a78ddb54fe8858ff82bf8fb67057cf7
Mikhail <mihail_ivanov007@mail.ru>
2023-08-02 17:41:45 +0300

    done 1,2,3 additional tasks

--- lesson_05/homework_03/game.cpp
+++ lesson_05/homework_03/game.cpp
@@ -47,6 +47,7 @@ bool write_to_score_board(const std::string &user_name, const int attempts) {
     }
     file << user_name << '\t' << attempts << std::endl;
     file.close();
+    return true;
 }
 
 bool check_str_in_arr(const std::string &external_str, const std::vector<std::string> &arr) {
@@ -59,33 +60,62 @@ bool check_str_in_arr(const std::string &external_str, const std::vector<std::st
     return false;
 }   
 
-void show_score_board() {
+bool show_score_board() {
     std::cout <<  "High scores table:" << std::endl;
     std::fstream file{name_file_scores, std::fstream::in};
     if (!file.is_open()) {
         std::cout << "Error! File was not opened!" << std::endl;
-        return;
+        return false;
     }
     std::string line;
     std::vector<std::string> names;
-    // std::map<std::string, int>
+    std::map<std::string, int> track_record; 
     while (getline(file, line)) {
         std::string user_name;
-        for (char ch : line) {
-            if (ch == '\t') {
-                break;
+        std::string attempts_str = "";
+        int attempts_int;
+        bool name_found = false;
+        std::isalpha(line[0]);
+
+        // проверка что Имя начинается с алфавитного символа
+        if (std::isspace(line[0])) {
+            return false;
+        }
+
+        for (int i = 0; i < line.size(); ++i) {
+            if (!name_found) {
+                if (line[i] != '\t') {
+                    user_name.push_back(line[i]);
+                } else {
+                    name_found = true;
+                }
+            } else {
+                if (line[i] == '\n') {
+                    break;
+                } else if (std::isdigit(line[i])) {
+                    attempts_str.push_back(line[i]);
+                } else {
+                    return 1;
+                }
             }
-            user_name.push_back(ch);
         }
-        bool res = check_str_in_arr(user_name, names);
-        if (!res) {
-            names.push_back(user_name);
+        if (attempts_str != "") {
+            attempts_int = std::stoi(attempts_str);
         }
-    }
-    for (std::string name : names) {
-        std::cout << name << '\n';
+        if (track_record.count(user_name)) {
+            if (track_record[user_name] > attempts_int) {
+                track_record[user_name] = attempts_int;
+            }
+        } else {
+            track_record[user_name] = attempts_int;
+        }
     }
     file.close();
+    for (auto [name, attempts] : track_record) {
+        std::cout << name << '\t' << attempts << '\n';
+    }
+    return true;
+
 }
 
 int get_arg(int argc, char* argv[]) {
@@ -121,7 +151,6 @@ int main(int argc, char *argv[]) {
     if (MAX_NUMBER == -1) {
         return -1;
     }
-        std::cout << "MAX_NUMBER = " << MAX_NUMBER << '\n';
     const int MIN_NUMBER = 0;
     int hidden_number = std::rand() % MAX_NUMBER;
     std::string user_name;
@@ -157,9 +186,14 @@ int main(int argc, char *argv[]) {
         
         if (user_number == hidden_number) {
             std::cout << "you win! attempts = " << attempts << std::endl;
-            write_to_score_board(user_name, attempts);
-
-            show_score_board();
+            if (!write_to_score_board(user_name, attempts)) {
+                std::cout << "Error! doesn't write score board" << '\n';
+                return 1;
+            }
+            if (!show_score_board()) {
+                std::cout << "Error! doesn't print score board" << '\n';
+                return 1;
+            }
             break;
         } else if(user_number < hidden_number) {
             std::cout << "greater then " << user_number << std::endl;
--- lesson_05/homework_03/test.cpp
+++ lesson_05/homework_03/test.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <format>
+// #include <format>
 #include <string>
 #include <fstream>
-#include <filesystem>
+// #include <filesystem>
 #include <vector>
 #include <map>
 
@@ -19,39 +19,69 @@ bool check_str_in_arr(const std::string &external_str, const std::vector<std::st
     return false;
 }   
 
-void show_score_board() {
+bool show_score_board() {
     std::cout <<  "High scores table:" << std::endl;
     std::fstream file{name_file_scores, std::fstream::in};
     if (!file.is_open()) {
         std::cout << "Error! File was not opened!" << std::endl;
-        return;
+        return false;
     }
     std::string line;
     std::vector<std::string> names;
-    std::map<std::string, int> scores; 
+    std::map<std::string, int> track_record; 
     while (getline(file, line)) {
         std::string user_name;
-        for (int i; i < line.size(); ++i) {
-            char ch = line[i];
-            if (ch != '\t') {
-                user_name.push_back(ch);
+        std::string attempts_str = "";
+        int attempts_int;
+        bool name_found = false;
+        std::isalpha(line[0]);
+
+        // проверка что Имя начинается с алфавитного символа
+        if (std::isspace(line[0])) {
+            return false;
+        }
+
+        for (int i = 0; i < line.size(); ++i) {
+            if (!name_found) {
+                if (line[i] != '\t') {
+                    user_name.push_back(line[i]);
+                } else {
+                    name_found = true;
+                }
+            } else {
+                if (line[i] == '\n') {
+                    break;
+                } else if (std::isdigit(line[i])) {
+                    attempts_str.push_back(line[i]);
+                } else {
+                    return 1;
+                }
             }
-            // std::cout << line[i+1] << '\n';
         }
-        bool res = check_str_in_arr(user_name, names);
-        if (!res) {
-            names.push_back(user_name);
+        if (attempts_str != "") {
+            attempts_int = std::stoi(attempts_str);
         }
-    }
-    for (std::string name : names) {
-        std::cout << name << '\n';
+        if (track_record.count(user_name)) {
+            if (track_record[user_name] > attempts_int) {
+                track_record[user_name] = attempts_int;
+            }
+        } else {
+            track_record[user_name] = attempts_int;
+        }
     }
     file.close();
+
+
+    for (auto [name, attempts] : track_record) {
+        std::cout << name << '\t' << attempts << '\n';
+    }
 }
 
 
 int main(int argc, char** argv) {
-    show_score_board();
+    if (!show_score_board())
+        std::cout << "Error! doesn't print score board" << '\n';
+        return 1;
     
 }
 
