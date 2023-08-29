7d4629b62c214d9eadd9895d87ddaf254c52f64a
MAIvanov <MAIvanov@t-argos.ru>
2023-08-29 18:17:35 +0300

    Done SerialConteiner

--- lesson_14/homework_06/main.cpp
+++ lesson_14/homework_06/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <vector>
 
 class SerialConteiner {
 public:
@@ -19,28 +20,62 @@ public:
 
     }
 
+    void insert(size_t index, int value) {
+        if (index > _size) throw std::string{"Error"};
+        ++_size;
+        int* new_space_data = new int[_size];
+        bool flag = false;
+        for (size_t i = 0; i < _size; ++i){ 
+            if (i == index) {
+                new_space_data[i] = value;
+                flag = true;
+                continue;
+            }
+            if (flag)
+                new_space_data[i] = _data[i - 1];
+            else
+                new_space_data[i] = _data[i];
+        }
+        delete[] _data;
+        _data = new_space_data;
+        new_space_data = nullptr;
+    }
+
+    void erase(size_t index) {
+        if (index > _size) throw std::string{"Error"};
+        --_size;
+        for (size_t i = 0; i < _size; ++i) {
+            if (i >= index) _data[i] = _data[i + 1];
+        }
+    }
+
     void print() {
         std::cout << '[';
         for (size_t i = 0; i < _size; ++i) {
-            std::cout << _data[i] << ", ";
+            std::cout << _data[i];
+            if (i != _size-1) std::cout << ", ";
         }
         std::cout << ']' << '\n';
 
     }
 
-    int get_value(size_t index) {
+    int get_value(size_t index) const {
         return _data[index];
     }
 
-    size_t get_size() {
+    size_t get_size() const {
         return _size;
     }
-
 
     ~SerialConteiner() {
         delete[] _data;
     }
 
+    int& operator[](int i) {
+        return _data[i];
+    }
+
+    // TODO как реализовать перегрузку  << внутри class
     // std::ostream& operator<<(std::ostream& stream, this) 
     // {
     //     stream << '[';
@@ -52,44 +87,48 @@ public:
 
     // }
 
-
 private:
     size_t _size;
     int* _data;
 };
 
 
-std::ostream& operator<<(std::ostream& stream, const SerialConteiner& list1) {
-        stream << list1.get_size();
-        
-        // stream << '[';
-        // for (size_t i = 0; i < list.get_size(); ++i) {
-        //     stream << list.get_value(i) << ", ";
-        // }
-        // stream << ']';
-        return stream;
-
+std::ostream& operator<<(std::ostream& stream, const SerialConteiner& my_list) {
+    stream << '[';
+    size_t size = my_list.get_size();
+    for (size_t i = 0; i < size; ++i) {
+        stream << my_list.get_value(i);
+        // проверка на последний символ
+        if (i != size-1) stream << ", ";
     }
+    stream << ']' << '\n';
+    return stream;
+
+}
 
 int main() {
-
-    // std::ostream& operator<<(std::ostream& stream, const SerialConteiner& list) 
-    // {
-    //     stream << '[';
-    //     for (size_t i = 0; i < list.get_size(); ++i) {
-    //         stream << list.get_value(i) << ", ";
-    //     }
-    //     stream << ']';
-    //     return stream;
-
-    // }
 
     SerialConteiner x;
     x.push_back(4);
     x.push_back(5);
-    x.push_back(42);
 
     x.print();
     std::cout << x;
 
+    int* ptr = &x[0];
+    std::cout << *ptr << '\n';
+    ++ptr;
+    std::cout << *ptr << '\n';
+
+    std::cout << "size: " << x.get_size() << '\n' << '\n';
+
+    x.insert(0, 9);
+    std::cout << x << "\n\n";
+
+    x.erase(2);
+    std::cout << x << "\n\n";
+
+ 
+
+    // std::array<int> a;
 }
